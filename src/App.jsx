79eb1dab7cef6345c3456cf1a7e1e4132b97bca2
MIguel Eduardo Ovando Alvarez <miguel-workspace@outlook.com>
2024-02-16 07:35:59 -0600

    init project

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+export default function App () {
+  return (
+    <main className='relative'>
+      Navbar
+      <section>
+        Hero
+      </section>
+      <section>
+        PopularProducts
+      </section>
+      <section>
+        SuperQuality
+      </section>
+      <section>
+        Services
+      </section>
+      <section>
+        SpecialOffer
+      </section>
+      <section>
+        CustomerReviews
+      </section>
+      <section>
+        Subscribe
+      </section>
+      <section>
+        Footer
+      </section>
+    </main>
+  )
+}
