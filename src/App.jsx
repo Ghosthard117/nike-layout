b0e1bc231226a5cf9282f2432f68c250ca9cfae5
MIguel Eduardo Ovando Alvarez <miguel-workspace@outlook.com>
2024-02-16 07:47:31 -0600

    Navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Navbar from './components/Navbar'
+
 export default function App () {
   return (
     <main className='relative'>
-      Navbar
+      <Navbar />
       <section>
         Hero
       </section>
